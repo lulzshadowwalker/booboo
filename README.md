a781e76db958969e40cbe1ba712c95f65dbe995c
lulzshadowwalker <lulzshadowwalker@gmail.com>
2023-09-11 22:23:58 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# booboo
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/dk/lulzshadowwalker/booboo/booboo/MainActivity.kt
@@ -0,0 +1,6 @@
+package dk.lulzshadowwalker.booboo.booboo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
